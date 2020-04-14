5f89f10b468ab929e5b10cd42c388917e73db363
Moritz Gnisia <moritzgnisia@gmail.com>
2020-04-14 12:41:31 +0200

    removed path of docker

--- Docker/build_container.sh
+++ Docker/build_container.sh
@@ -8,8 +8,8 @@
 [ -z "$sys" ] && read -p "Which image would you like to build? type: arch or ubuntu: " sys
 
 # Build Container
-[ "$sys" = "Ubuntu" ] && /usr/bin/docker build -f Ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
-[ "$sys" = "Arch" ] && /usr/bin/docker build -f Arch.Dockerfile -t nas_docker:arch . && exit 0
+[ "$sys" = "Ubuntu" ] && docker build -f Ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
+[ "$sys" = "Arch" ] && docker build -f Arch.Dockerfile -t nas_docker:arch . && exit 0
 
 echo "The provided image doesn't match with arch or ubuntu" && exit 1
 
