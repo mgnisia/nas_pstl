bd9bec34572a847ddafa430bc95d6e822615b9f0
Moritz Gnisia <moritzgnisia@gmail.com>
2020-04-14 12:38:01 +0200

    updating shell script for building containers

--- Docker/build_container.sh
+++ Docker/build_container.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+# Usage ./build_container.sh ubuntu or interactive ./build_container.sh
+
+# Set input to sys
+[ -z "$1" ]; sys=$1 
 
 # Decision which image to build
-read -p "Which image would you like to build? type: arch or ubuntu: " sys
-
-# Test if sys variable is empty
-[ -z "$sys" ] && echo "provided string is empty" && exit 1
+[ -z "$sys" ] && read -p "Which image would you like to build? type: arch or ubuntu: " sys
 
 # Build Container
-[ "$sys" = "ubuntu" ] && /usr/bin/docker build -f ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
-[ "$sys" = "arch" ] && /usr/bin/docker build -f arch.Dockerfile -t nas_docker:arch . && exit 0
+[ "$sys" = "ubuntu" ] && /usr/bin/docker build -f Ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
+[ "$sys" = "arch" ] && /usr/bin/docker build -f Arch.Dockerfile -t nas_docker:arch . && exit 0
 
 echo "The provided image doesn't match with arch or ubuntu" && exit 1
 
