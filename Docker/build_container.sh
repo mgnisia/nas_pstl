9d5aa947871ed1d902ba72ed821ad03dbfed7ec8
Moritz Gnisia <moritzgnisia@gmail.com>
2020-04-14 12:39:47 +0200

    updating script for capital letters

--- Docker/build_container.sh
+++ Docker/build_container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Usage ./build_container.sh ubuntu or interactive ./build_container.sh
+# Usage ./build_container.sh Ubuntu or interactive ./build_container.sh
 
 # Set input to sys
 [ -z "$1" ]; sys=$1 
@@ -8,8 +8,8 @@
 [ -z "$sys" ] && read -p "Which image would you like to build? type: arch or ubuntu: " sys
 
 # Build Container
-[ "$sys" = "ubuntu" ] && /usr/bin/docker build -f Ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
-[ "$sys" = "arch" ] && /usr/bin/docker build -f Arch.Dockerfile -t nas_docker:arch . && exit 0
+[ "$sys" = "Ubuntu" ] && /usr/bin/docker build -f Ubuntu.Dockerfile -t nas_docker:ubuntu . && exit 0
+[ "$sys" = "Arch" ] && /usr/bin/docker build -f Arch.Dockerfile -t nas_docker:arch . && exit 0
 
 echo "The provided image doesn't match with arch or ubuntu" && exit 1
 
